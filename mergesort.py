322a47ca1f21daac1839b012fa2b816ae9a124bc
Mohanpraveen <38327559+hazamp01@users.noreply.github.com>
2020-10-23 13:15:29 -0700

    Delete mergesort.py

--- mergesort.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-       for i in nums:
-           if nums[i]==val:
-               nums.remove(i)
-
-a=Solution()
-a.removeElement([0,1,2,2,3, 0,4,2], 2)
